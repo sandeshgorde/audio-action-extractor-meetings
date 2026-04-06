efaa6c1f979fc3e00492182519bd1c624f826735
sandeshgorde <sandeshgorde948@gmail.com>
2026-04-07 01:56:39 +0530

    Fix: Update Dockerfile pip install for Render deployment

--- backend/src/main/java/com/audioextractor/service/GroqService.java
+++ backend/src/main/java/com/audioextractor/service/GroqService.java
@@ -4,7 +4,6 @@ import com.audioextractor.exception.AudioProcessingException;
 import com.audioextractor.exception.AudioProcessingException.ErrorCode;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
